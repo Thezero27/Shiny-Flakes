80eb96b09107831ab3ff060647da253d75cd9add
Thezero <67761713+Thezero27@users.noreply.github.com>
2021-11-17 15:22:17 -0500

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -12,11 +12,11 @@ const getValueInput = () => {
     if (usuario == usrot && pass == passt) {
         //var aviso = document.getElementById("error-login")
         document.getElementById("error-login1").innerHTML = "Login correcto";
-
+        open("Shiny-Flakes/home.html")
         //para trabajar...si terminas y vas a subir al servidor borralo
         //open("/TRABAJOS%20MIGUEL/PAGINAS%20WEB/HTML5/Tienda/Drogas/home.html")
         //Desbloquea es para subir al servidor
-        open("Shiny-Flakes/home.html")
+        
         //open("/home.html")
     }
     if (usuario != usrot) {
